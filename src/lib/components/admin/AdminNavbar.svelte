fea1ab885ae0ceb515ab031b79da42a1456e1970
Fedor_Semochkin <221800ss@gmail.com>
2024-06-01 11:05:54 +0700

    update main page

--- src/lib/components/admin/AdminNavbar.svelte
+++ src/lib/components/admin/AdminNavbar.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <nav
-	class="border-b-2 border-slate-200 w-full flex text-4xl items-center h-20 p-10 bg-slate-100 cursor-pointer"
+	class="border-b-2 border-slate-200 w-full flex text-4xl items-center pl-4 h-24 p-1 bg-slate-100 cursor-pointer"
 >
-	<div on:click={() => goto('/admin/categories/new')}>LOGO</div>
+	<div on:click={() => goto('/admin/categories/new')}>
+		<img src="/assets/logo.svg" alt="Logo" class="w-24 h-24" />
+	</div>
 </nav>
